5e794217771eb6eee6ee3071a70149055736afe8
soukaina203 <soukainamourabit9@gmail.com>
2024-06-25 17:53:22 +0100

    updates

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -18,7 +18,7 @@ export class SkillsComponent {
 skills=[
   {img:"frontend",title:"Frontend Techs",techs:"HTML , CSS , JavaScript , TailwindCss ,ReactJs , Angular"},
   {img:"backend",title:"Backend Techs",techs:"PHP , Laravel , C# , .Net"},
-  {img:"tools",title:"Other Techs",techs:" VS Code , Figma ,Git ,Github ,Responsive Design"},
+  {img:"tools",title:"Other Techs",techs:" VS Code , Figma ,Git ,Github ,Responsive Design, Scrum"},
   {img:"data2",title:"Database Techs",techs:"Mysql , Postgres , MongoDB"},
 
 ]
